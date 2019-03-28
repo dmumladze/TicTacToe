d687b59b980dc3e97b3a9580a316f554469cd124
David Mumladze <davidlars99@gmail.com>
2019-03-28 12:47:50 -0400

    AI improvement

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from board  import Board
 from game   import Game
 from player import Human, Computer
 
-board = Board()
-game  = Game(board)
+game = Game()
 
-game.accept_player(Human('0'))
-game.accept_player(Computer('X'))
+game.accept_player(Human())
+game.accept_player(Computer())
 
 game.start()            
