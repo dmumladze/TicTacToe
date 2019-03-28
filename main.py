55eda97419ef0f13a5e0dfdc5e2b95cbde1e9079
David Mumladze <davidlars99@gmail.com>
2019-03-28 13:02:36 -0400

    AI improvements

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from player import Human, Computer
 
 game = Game()
 
+''' AI against AI
+game.accept_player(Computer())
+game.accept_player(Computer())
+'''
+
+''' Human against AI '''
 game.accept_player(Human())
 game.accept_player(Computer())
 
-game.start()            
+game.start()  
+         
